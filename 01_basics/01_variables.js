c1e430295b179443fa946b7e9a9a36489b6bda64
GautamRawat26 <gautam.rawat.2019@gmail.com>
2025-03-11 06:06:25 +0000

    use of variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "Gautam.rawat.2019@gmail.com"
+var accountPass = "Gautam26"
+accountCity = "Haridwar"
+let accountHolder;
+
+console.table([accountId , accountEmail , accountPass , accountCity, accountHolder])
