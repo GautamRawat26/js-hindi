74e104d276de88bae6e83a32a0243ccc8f44bbb6
GautamRawat26 <gautam.rawat.2019@gmail.com>
2025-03-11 08:10:00 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert(3+3) // shows error in code editor because we are using nodejs, not browser 
+
+let name = "Gautam"
+let age = 21
+let isLoggedin = false
+
+//PRIMITIVE DATA TYPES
+// number => 2 to the power 53
+// bigint
+// string
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => to find uniqueness
+//Immutable, stored by value (e.g., String, Number, BigInt, Boolean, undefined, null).
+
+//NON PRIMITIVE
+//Object
+// let person = { name: "John", 
+//                age: 30 };
+
+//Array
+//let numbers = [1, 2, 3, 4];
+
+//Functions
+/*function greet() {
+    console.log("Hello");
+}
+*/
+//Mutable, stored by reference (e.g., Object, Array, Function).
+
+//Determine the type of Data Types
+console.log(typeof undefined)//Undefined
+console.log(typeof null)//Object
