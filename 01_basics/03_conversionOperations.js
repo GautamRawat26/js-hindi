4f3188647b51f6754859791c69c6ae5159c57c23
GautamRawat26 <gautam.rawat.2019@gmail.com>
2025-03-12 06:47:59 +0000

    conversion and operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,3 +1,4 @@
+//Conversion
 let score = "33"
 console.log(typeof score); //String
 
@@ -16,3 +17,34 @@ console.log(valueInScore2); //NaN(Not a Number)
 // 1 => True ; 0 => False
 // "" => False
 // "Gautam" => True
+
+
+
+//OPERATIONS
+
+let value = 3
+let negValue = -value
+console.log(negValue) // -3
+
+// console.log(2+2) //add
+// console.log(2-2) //Subs
+// console.log(2*2) //Multiply
+// console.log(2/2) //Divide
+// console.log(2%2) //Modulous
+// console.log(2**2) //Power
+
+let str1 = "Gautam"
+let str2 = " Rawat"
+
+let str3 = str1 + str2
+console.log(str3); //Gautam Rawat
+
+console.log("1" + 2) //12
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") //32
+console.log(1 + "2" + 2) //122
+
+
+let count = 100
+++count ;
+console.log(count)
