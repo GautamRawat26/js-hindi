eb86920f96af2e1691e0169166fc311e0cbd1c31
GautamRawat26 <gautam.rawat.2019@gmail.com>
2025-03-16 15:40:19 +0000

    dates

---
+++ 01_basics/07_date.js
@@ -0,0 +1,10 @@
+//Dates
+
+let myDate = new Date();
+console.log(myDate); // 2025-03-16T15:35:30.334Z
+console.log(myDate.toDateString());//Sun Mar 16 2025
+console.log(myDate.toString());//Sun Mar 16 2025 15:35:30 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toISOString());//2025-03-16T15:35:30.334Z
+console.log(myDate.toLocaleDateString());// 3/16/2025
+
+
